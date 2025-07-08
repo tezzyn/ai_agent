359138ff91a9cc391f7ef0cddb1db55300519a34
tezzytech <qurteznealy@gmail.com>
2025-07-07 20:03:00 -0400

    pc

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
+
 def main():
-    print("Hello from ai-agent!")
+    #print("Hello from ai-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', 
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+    )
+
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.prompt_token_count}")
+    
 
 
 if __name__ == "__main__":
