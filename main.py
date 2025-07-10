11c93297dff3a3bc4dd3eabee5cea0ae516d017f
tezzytech <qurteznealy@gmail.com>
2025-07-09 20:36:16 -0400

    why am i getting folded by verbose?

--- main.py
+++ main.py
@@ -7,28 +7,61 @@ from google.genai import types
 
 def main():
     #print("Hello from ai-agent!")
+
     load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
-    client = genai.Client(api_key=api_key)
 
-    response = client.models.generate_content(
-        model='gemini-2.0-flash-001', 
-        contents=sys.argv
-        
-    )
+    args = sys.argv[1:]
 
-
-    if len(sys.argv) <= 1:
+    if not args:
             print("invalid input")
             sys.exit(1)
 
+    user_prompt = " ".join(args)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    user_prompt = " ".join(args)
+
     messages = [
-    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+ 
     ]
 
-    print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.prompt_token_count}")
+    
+         
+
+    generate_content(client, messages)
+    
+
+def generate_content(client, messages):
+    
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', 
+        contents=messages,
+    
+    )
+
+    print(f"~~~~{len(messages[0])}~~~~")
+
+
+    if "--verbose" in messages:
+
+        print(f"User prompt:\n{response.text}")
+        #print(response.text)
+
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    # print(response.text)
+    # print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    # print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+
+
+#print(response.text)
+#print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+#print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     
 
 
