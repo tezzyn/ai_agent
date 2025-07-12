b7dbcb6136273607e899160a5086594ed9fbc464
tezzytech <qurteznealy@gmail.com>
2025-07-12 10:46:53 -0400

    fresh commit

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,19 +2,41 @@ import os
 
 
 def get_files_info(working_directory, directory=None):
-    os.path.join(working_directory, directory)
+    # try:
 
-    if directory not in working_directory:
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    
-    if os.path.isdir(directory):
-        return f'Error: "{directory}" is not a directory'
+    #     path = os.path.join(working_directory, directory)
 
-    
-    
-    contents = (
-        f"- {directory}: file_size={os.path.getsize(directory)} bytes, is_dir={os.path.isdir(directory)}"
-    )
-
-    return contents
+    #     print(os.path.abspath(path))
         
+
+    #     if directory not in path:
+    #         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        
+    #     if not os.path.isdir(directory):
+    #         return f'Error: "{directory}" is not a directory'
+
+         
+        
+    #     contents = (
+    #         f"- {directory}: file_size={os.path.getsize(directory)} bytes, is_dir={os.path.isdir(directory)}\n"
+    #     )
+
+    #     return contents
+    # except OSError as e:
+    #     print(f"Error: {e}")
+
+ 
+
+    path = os.path.join(working_directory,directory)
+
+    full_path = os.listdir(path)
+
+    try:
+
+        for i in full_path:
+            contents = (
+                f"- {i}: file_size={os.path.getsize(i)} bytes, is_dir={os.path.isdir(i)}\n"
+            )
+        return contents
+    except OSError as e:
+        print(f"Error: {e}")
