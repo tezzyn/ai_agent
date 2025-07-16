a60e242175f898ba6f1a2492717999f44068551a
tezzytech <qurteznealy@gmail.com>
2025-07-15 21:37:01 -0400

    stuck

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -4,28 +4,35 @@ from pathlib import Path
 
 def get_files_info(working_directory, directory=None):
 
-
     wd = Path(working_directory)
     d = Path(directory)
 
     path = wd / d
 
-    def content(path):
-        prompt = []
+    prompt = []
+
+    rel_check = (wd.absolute().parent == d.absolute())
+
+    print(d)
+
+    print(d.absolute())
+    print(d.resolve())
+    # print(rel_check)
+
+    if d.is_dir() == False:
+        return f'Error: "{directory}" is not a directory'
+
+    if directory == "." or rel_check == True:
 
         for i in path.iterdir():
-            print(i)
-            try:
-            
-                contents = (
-                    f"- {i.name}: file_size={i.stat().st_size} bytes, is_dir={i.is_dir()}"
-                )
-                prompt.append(contents)
+            #print(type(i))
 
-            except OSError as e: 
-                
-                print(f"Error: {e}")
-                
-        return prompt
+            contents = (
+                f"- {i.name}: file_size={i.stat().st_size} bytes, is_dir={i.is_dir()}"
+            )
+            prompt.append(contents)    
+    else:
 
-    return "\n".join(content(path))
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    
+    return "\n".join(prompt)
