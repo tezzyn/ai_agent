3d3e80484e4014e40b1ce9e13c95e6c0deaa8f11
tezzytech <qurteznealy@gmail.com>
2025-07-26 18:34:36 -0400

    fresh commit

---
+++ functions/write_file.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+
+
+
+
+def write_file(working_directory, file_path, content):
+
+    combined = Path(working_directory)/Path(file_path)
+
+    cwd = Path.cwd()
+
+    rel_check = ( 
+        
+        Path(file_path).resolve().is_relative_to(cwd)
+        
+        )
+
+    if not rel_check:
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+    try:
+
+        with open(combined, "r+") as f:
+
+            return ()
+
+        f.close()
+
+    except ValueError as e:
+        return f'Error: "{e}"'
