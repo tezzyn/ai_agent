64b98a02bed11911ab667105d357a3eaceed906f
tezzytech <qurteznealy@gmail.com>
2025-08-20 01:08:44 -0400

    foward progress, stdout, stderr

--- functions/run_file.py
+++ functions/run_file.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import subprocess
 import time
 import sys
-from subprocess import Popen, SubprocessError
+from subprocess import SubprocessError
 
 
 def run_python_file(working_directory, file_path, args=[]):
@@ -10,7 +10,6 @@ def run_python_file(working_directory, file_path, args=[]):
     combined = Path(working_directory)/Path(file_path)
 
     cwd = Path.cwd()
-    #print(cwd)
 
     rel_check = ( 
         
@@ -26,9 +25,6 @@ def run_python_file(working_directory, file_path, args=[]):
     
     if not file_path.endswith('.py'):
         return f'Error: "{file_path}" is not a Python file.'
-
-
-    # print(Path(file_path).resolve().is_relative_to(cwd))
     
 
     prompt = []
@@ -38,28 +34,44 @@ def run_python_file(working_directory, file_path, args=[]):
     else:
         prompt = ["uv", "run",f"{combined}"]
 
-   
     try:
    
         process = subprocess.run(
             prompt, 
-            #capture_output=True, 
-            #shell=True, 
-            # stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE, 
-            stderr=subprocess.PIPE,
+            capture_output=True, 
+            # shell=True, 
             text=True,
-            timeout=20,
+            timeout=5,
             check=True,
             )
+        
+        
+        
+
+        # if process.returncode != 0:
+
+        #     return f"STDERR:\n {process.stderr}"
+        # elif process.stdout != '':
+        #     return f"STDOUT:\n {process.stdout}" 
+        # else:
+        #     return "No output produced."
 
         
-        # print(process.stdout)
-        #print(process.stderr)
+        #print(process.stdout)
+        # print(process.stderr)
     except subprocess.SubprocessError as e:
         return(e)
+    
+     
+    if process.returncode != 0:
 
-    return process.stdout
+        return f"STDERR:\n {process.stderr}"
+    elif process.stdout != '':
+        return f"STDOUT:\n {process.stdout}" 
+    else:
+        return "No output produced."
+
+
     
 
 
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 
 
+import time
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 from functions.run_file import run_python_file
@@ -64,11 +65,12 @@ import subprocess
 
 # print("--------------------------------------------------------------------")
 
-# print(run_python_file("calculator", "main.py", ["3 + 5"]))
+print(run_python_file("calculator", "main.py", ["3 + 5"]))
+
 
 # print("--------------------------------------------------------------------")
 
-print(run_python_file("calculator", "tests.py"))
+# print(run_python_file("calculator", "tests.py"))
 
 # print("--------------------------------------------------------------------")
 
